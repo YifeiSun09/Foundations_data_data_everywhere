1aa21c001ff74d99de3c29a68ea42da349b3f8e1
yifeisun <yirki99@yahoo.co.jp>
2021-03-29 08:31:05 +0900

    theses two

---
+++ TAKE OUR DIAGNOSITC QUIZ.py
@@ -0,0 +1,85 @@
+1.Which of the following items are examples of data? Select all that apply.
+A.Numbers
+B.Words
+C.Observations
+
+2.A clothing retailer collects and stores data about its sales revenue.
+Which of the following would be part of its data ecosystem?
+Select all that apply.
+A.Records of its inventory
+B.The cloud that store its database
+C.The database of sales revenue
+
+3.In data analytics, how is data described?
+B.A collection of facts
+
+4.What is the process of guiding business strategy using facts?
+D.Data-driven decision-making
+
+5.Fill in the blank: Curiosity, understanding context, having a technical
+mindset, data design, and data strategy are _____. They enable data
+analysts to solve problems using facts.
+B.analytical skills
+
+6.The owner of a skate shop notices that every time a certain employee
+has a shift, there are higher sales numbers at the end of the day.
+After some investigation, the owner realizes that since the employee
+was hired, the store earns 15% more each month. In this scenario,
+the manager used which quality of analytical thinking?
+C.Correlation
+
+7.When a shipment arrives late from a supplier, a data analyst asks
+themselves, “Why?” five times in a row. What are they trying to do?
+D.Identify the root cause of the problem
+
+8.Gap analysis is a process that could help accomplish which of the
+following tasks? Select all that apply.
+A.Increase the efficiency of a car manufacturer based on its current
+assembly process
+B.Improve accessibility for an educational app based on its current
+functionality
+C.Reduce a company’s carbon footprint based on its current emissions
+
+9.An analysis firm is conducting a data analysis of rainwater trends to
+give recommendations to agricultural companies. When they reach the
+manage stage of the data life cycle, which of the following might they do?
+Select all that apply
+A.Decide which security tools they will use to protect it
+B.Store and clean the data they have collected
+
+10.An advertising firm has used insights from its analytics team to create
+a strategy for improving sales. Now, they implement a plan to increase
+annual revenue. The firm is at which step of the data analysis process?
+D.Act
+
+11.The analyze phase of the data life cycle includes which of the
+following steps? Select all that apply.
+A.Using a formula to perform calculations
+B.Creating a report from data
+C.Using spreadsheets to aggregate data
+
+12.A data analyst adds descriptive headers to columns of data in a
+spreadsheet. How does this improve the spreadsheet?
+A.
+It adds context
+
+13.SORT range
+
+
+14.You are querying a school database. The column name for customer
+identification numbers is Student_ID. What is the correct WHERE clause
+to retrieve data about the student with identification number (ID)
+8885317? Type only the WHERE clause, beginning with the word WHERE.
+Type your answer below.
+
+15.You are writing a SQL query to analyze data about recent trampoline
+sales. The Store_sales table describes the number of trampolines sold at
+several sporting goods stores. What is the correct FROM clause to retrieve
+data from this table? Type only the FROM clause, beginning with the word
+FROM. Type your answer below.
+
+16.Question 16
+Imagine you are sharing your data with a friend. Why might you display data
+with a data visualization instead of a table? Select all that apply.
+A.It’s easy to understand
+B.It’s aesthetically pleasing
---
+++ main.py
@@ -0,0 +1,107 @@
+*Weekly challenge 1*
+1.The collection, transformation,
+and organization of data in order to draw conclusions,
+make predictions, and drive informed decision-making describes what?
+RIGHT:
+DATA ANALYSIS
+
+2.Which of the following are components of people analytics?
+Select all that apply.
+RIGHT:
+A.Gaining insights that help create a more productive workplace
+B.Collecting and analyzing information on employees
+
+3.In data analytics,
+what term describes a collection of elements that interact
+with one another?
+RIGHT:
+A data ecosystem
+
+4.What tactics can a data analyst use to effectively blend gut
+instinct with facts? Select all that apply.
+RIGHT:
+Use their knowledge of how their company works to better understand
+a business need.
+
+5.A company defines a problem it wants to solve.
+Then, a data analyst gathers relevant data, analyzes it, and uses it
+to draw conclusions. The analyst shares their analysis with
+subject-matter experts, who validate the findings. Finally,
+a plan is put into action. What does this scenario describe?
+RIGHT:
+Data-driven decision-making
+
+6.To get the most out of data-driven decision-making, it’s important
+to include insights from people very familiar with the business problem.
+Identify what these people are called.
+RIGHT:
+Subject-matter experts
+
+7.After completing their analysis, data analysts often share their
+results with subject-matter experts. These people offer additional
+insights into the business problem, identify any inconsistencies in
+the analysis, and validate the choices being made.
+RIGHT:
+True
+
+8.You have recently subscribed to an online data analytics magazine.
+You really enjoyed an article and want to share it in the discussion forum.
+Which of the following would be appropriate in a post? Select all that
+apply.
+RIGHT:
+A.Giving credit to the original author
+B.Checking your post for typos or grammatical errors
+C.Including your own thoughts about the article
+
+
+
+----ANOTHER----
+1.Which of the following options describes data analysis?
+RIGH：
+The collection, transformation, and organization of data
+in order to draw conclusions, make predictions, and drive informed
+decision-making
+
+2.People analytics is used to create a more productive workplace.
+It involves collecting and analyzing information about what?
+RIGHT:
+Employees
+
+3.Fill in the blank: Data ecosystems are made up of elements
+that _____ with each other. This makes it possible for them to produce,
+manage, store, organize, analyze, and share data.
+RIGHT:
+interact
+
+4.Select the best description of gut instinct.
+RIGHT:
+An intuitive understanding of something with little
+or no explanation
+
+5.A furniture manufacturer wants to find a more environmentally friendly
+way to make its products. A data analyst helps solve this problem by
+gathering relevant data, analyzing it, and using it to draw conclusions.
+The analyst then shares their analysis with subject-matter experts from
+the manufacturing team, who validate the findings. Finally, a plan is put
+into action. This scenario describes data science.
+RIGHT:
+False
+
+6.To get the most out of data-driven decision-making, it’s important to
+include insights from people very familiar with the business problem.
+Identify what these people are called.
+RIGHT:
+Subject-matter experts
+
+7.After completing their analysis, data analysts often share their results
+with subject-matter experts. These people offer additional insights into
+the business problem, identify any inconsistencies in the analysis,
+and validate the choices being made.
+RIGHT:True
+
+8.You read an interesting article about data analytics in a magazine and
+want to share some ideas from the article in the discussion forum.
+In your post, you include the author and a link to the original article.
+This would be an inappropriate use of the forum.
+RIGHT:
+False
