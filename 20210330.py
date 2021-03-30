ff7bd080fec3aff92b1b7127a71f5e373affb207
yifeisun <yirki99@yahoo.co.jp>
2021-03-30 22:26:32 +0900

    something not important

---
+++ 20210330.py
@@ -0,0 +1,21 @@
+1.The qualities and characteristics associated with
+solving problems using facts
+    Analytical skills
+
+2.The analytical skill that involves breaking
+processes down into smaller steps and working
+with them in an orderly, logical way
+    A technical mindset
+
+3.Analytical skills that involve how you organize
+information
+    Data design
+
+4.The analytical skill that has to do with how you
+group things into categories
+    Understanding context
+
+5.The analytical skill that involves managing the
+processes and tools used in data analysis
+    Data strategy
+
