d63cc9c2c2e5a3adaf5b00265eae6d524b3b7c27
yifeisun <yirki99@yahoo.co.jp>
2021-03-31 07:58:52 +0900

    oop dog and cat

---
+++ dog_cat.py
@@ -0,0 +1,29 @@
+class Pet:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} years old")
+    def speak(self):
+        print("I don't know what to say")
+
+class Cat(Pet):
+    def __init__(self,name,age,color):
+        super().__init__(name,age)
+        self.color=color
+
+    def speak(self):
+        print('Meow')
+
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} years old I am {self.color}")
+
+class Dog(Pet):
+    def speak(self):
+        print('Bark')
+
+p=Cat("Beibei",19,"brown")
+p.show()
+p.speak()
+
